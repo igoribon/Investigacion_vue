d4d9dd56bd1223a743c1ac22b82e4332eb02516e
Varushet <detlar99@gmail.com>
2025-02-10 12:06:27 +0100

    buff ni sé qué estoy haciendo, diosito ayudame x2

---
+++ src/components/Alejandro.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" placeholder="Escribe tu nombre aqui!!" v-model="nombre">
+        <h2> {{ saludo }} {{ nombre }}!!</h2>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup>
+import {ref, computed} from 'vue'
+
+const nombre 
+
+// const saludo = [ mañana, tarde, noche]
+// const mañana = "Buenos días"
+// const tarde = "Buenas tardes"
+// const noche = "Buenas Noches Zzz"
+
+</script>
