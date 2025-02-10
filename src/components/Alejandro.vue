7637a3498d033536452e2a8f50fdaeadf772e258
Varushet <detlar99@gmail.com>
2025-02-10 12:57:27 +0100

    makinawer

--- src/components/Alejandro.vue
+++ src/components/Alejandro.vue
@@ -2,21 +2,52 @@
     <div>
         <input type="text" placeholder="Escribe tu nombre aqui!!" v-model="nombre">
         <h2> {{ saludo }} {{ nombre }}!!</h2>
+        <h3>Son las {{ horaActual }} {{ meridiano }}</h3>
     </div>
 </template>
 
 <style scoped>
-
+div {
+    padding: 20px;
+    text-align: center;
+}
+input {
+    padding: 8px;
+    margin-bottom: 10px;
+}
+h2 {
+    color: #276cb1;
+}
 </style>
 
 <script setup>
-import {ref, computed} from 'vue'
+import {ref, computed} from 'vue';
 
-const nombre 
+const nombre = ref(' ');
+const horaActual = new Date().getHours();
 
-// const saludo = [ mañana, tarde, noche]
-// const mañana = "Buenos días"
-// const tarde = "Buenas tardes"
-// const noche = "Buenas Noches Zzz"
+const saludo = computed(() => {
+
+    if (horaActual >= 5 && horaActual <  12){
+        return 'Buenos días';
+    } else if (horaActual >= 12 && horaActual < 20){
+        return 'Buenas tardes';
+    } else {
+        return 'Buenas noches Zzz';
+    }
+});
+
+const meridiano = computed(() => {
+
+    if (horaActual >= 12 && horaActual){
+        return 'am'
+    } else {
+        return 'pm'
+    }
+});
+
+
+
+
 
 </script>
