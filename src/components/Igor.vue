c91cbeefe0ea355849388a06d9ed8a85c89d40b2
igoribon <igoribon@gmail.com>
2025-02-13 18:58:24 +0100

    Puesto una comunicación padre-hijo

--- src/components/Igor.vue
+++ src/components/Igor.vue
@@ -9,26 +9,48 @@
   <div v-if="contador < 0">Escribe una cantidad mayor que 0</div>
 
   <!-- Si usamos un v-else, se mostrará si la condición del v-if no se cumple. Tiene que ir siempre después de un v-if. -->
-  <div v-else-if="contador >= 5">Cantidad menor que cinco</div>
+  <div v-else-if="contador >= 5">Cantidad mayor que cinco</div>
 
   <div v-show="comprobacion">No está permitido el número 0</div>
+
+  <!-- <div>Total de clicks: {{ totalClicks }}</div>
+  <div>Suma de valores: {{ sumValores }}</div> -->
+
+  <!-- Aquí tenemos el botón que mostrará los valores: El número de veces que se pulsan los botones y la suma de todos los valores que se muestran. El evento on-click hace que mostrarvalores se cambie a true o false según si está activo o no. -->
+  <button class="boton" @click="mostrarValores = !mostrarValores">
+    {{ mostrarValores ? 'Ocultar valores' : 'Mostrar valores' }}
+  </button>
+  <!-- Aquí mostramos el complemento hijo de la suma de los valores: {{ sumValores }} -->
+  <ValoresDisplay v-if="mostrarValores" :totalClicks="totalClicks" :sumValores="sumValores" />
 </template>
 
 <script setup>
 // Contador
 import { computed, ref } from "vue";
+//Importamos el complemento hijo para que pueda capturar los valores de este componente
+import ValoresDisplay from "./Valores.vue";
 
 const contador = ref(0);
+//Aquí definimos las variables de totalClicks y sumValores que se usarán en el componente hijo. También definimos mostrarValores que se usará para mostrar u ocultar los valores.
+const totalClicks = ref(0);
+const sumValores = ref(0);
+const mostrarValores = ref(false);
 
 //Funciones para incrementar y decrementar el contador que luego llamamos desde los votones con @click. También le hemos añadido un color al contador según si es positivo o negativo con un if
 const decremento = () => {
   contador.value--;
+  //Sumamos 1 al valor totalClicks y sumamos el valor del contador al sumValores. 
+  totalClicks.value++;
+  sumValores.value += contador.value;
   if (contador.value < 0) {
     contColor.value = "rojo";
   }
 };
 const incremento = () => {
   contador.value++;
+   //Sumamos 1 al valor totalClicks y sumamos el valor del contador al sumValores. 
+  totalClicks.value++;
+  sumValores.value += contador.value;
   if (contador.value > 0) {
     contColor.value = "verde";
   }
---
+++ src/components/valores.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Tenemos el div con los datos que queremos mostrar -->
+    <div>
+      <p>Total de clicks: {{ totalClicks }}</p>
+      <p>Suma de valores: {{ sumValores }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  //Importamos el módulo para las propiedades reactivas. En el hijo debe ser toRefs porque estamos trayendo coonstantes reactivas de otro sitio
+  import { toRefs } from "vue";
+  
+  // Recibimos las propiedades desde el componente padre a través de un prop. 
+  const props = defineProps({
+    totalClicks: Number,
+    sumValores: Number,
+  });
+  </script>
+  
+  <style scoped>
+  p {
+    font-size: 1.5rem;
+  }
+  </style>
